e90dca62f64da6af44d47d63bb59e66577949eb4
Guilcher Pascal <pgu.dev@gmail.com>
2011-09-21 22:30:17 +0200

    update start stop buttons

--- src/com/pgu/client/ui/StartButton.java
+++ src/com/pgu/client/ui/StartButton.java
@@ -1,6 +1,8 @@
 package com.pgu.client.ui;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
@@ -23,8 +25,17 @@ public class StartButton extends Composite {
     public StartButton() {
         initWidget(uiBinder.createAndBindUi(this));
 
-        UiHelper.id("container-start", container);
-        UiHelper.id("start", start);
+        UiHelper.id("container-start-btn", container);
+        UiHelper.id("start-btn", start);
+
+        container.addDomHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(final ClickEvent event) {
+                GWT.log("start...");
+                // TODO PGU turn on the cards
+            }
+        }, ClickEvent.getType());
     }
 
 }
--- src/com/pgu/client/ui/StopButton.java
+++ src/com/pgu/client/ui/StopButton.java
@@ -1,6 +1,8 @@
 package com.pgu.client.ui;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
@@ -23,8 +25,17 @@ public class StopButton extends Composite {
     public StopButton() {
         initWidget(uiBinder.createAndBindUi(this));
 
-        UiHelper.id("container-stop", container);
-        UiHelper.id("stop", stop);
+        UiHelper.id("container-stop-btn", container);
+        UiHelper.id("stop-btn", stop);
+
+        container.addDomHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(final ClickEvent event) {
+                GWT.log("stop...");
+                // TODO PGU turn off the cards
+            }
+        }, ClickEvent.getType());
 
     }
 
